15bd5b16da61bad39b395b5adf05c24fe0093b10
Ayarachi Fuentes Hector Daniel <mp4o@hotmail.com>
2026-04-28 20:24:44 -0300

    chore: remove unused voice synthesis test artifacts and dependencies

--- scratch/check_fs.js
+++
@@ -1,9 +0,0 @@
-const checkFS = () => {
-    try {
-        const stat = Module.FS.stat('/tokens.txt');
-        console.log('tokens.txt size:', stat.size);
-    } catch(e) {
-        console.log('tokens.txt error:', e);
-    }
-};
-checkFS();
